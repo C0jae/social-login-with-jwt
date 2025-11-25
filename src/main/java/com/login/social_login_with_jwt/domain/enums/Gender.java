c357c6a2c7333e2973e702d852e2a12fce6ad7f0
NB-CYJ <cyj@newbird.co.kr>
2025-11-25 18:15:09 +0900

    feat : 도메인관련 enum 추가

---
+++ src/main/java/com/login/social_login_with_jwt/domain/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.login.social_login_with_jwt.domain.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
---
+++ src/main/java/com/login/social_login_with_jwt/domain/enums/SocialProvider.java
@@ -0,0 +1,7 @@
+package com.login.social_login_with_jwt.domain.enums;
+
+public enum SocialProvider {
+    GOOGLE,
+    KAKAO,
+    NAVER
+}
